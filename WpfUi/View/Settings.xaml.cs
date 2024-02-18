4ac640250d752ec0f4076b302ebc69a1efd5e40a
IYugo <hw0383@outlook.com>
2024-02-18 21:01:04 +0900

    xaml 파일 생성

--- WpfUi/View/Settings.xaml.cs
+++ WpfUi/View/Settings.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfUi {
+namespace WpfUi.View {
     /// <summary>
     /// Interaction logic for Settings.xaml
     /// </summary>
