e2cffebe72dd8c40ebb806ffc534e18cefebe40e
Kchanghyun <kch1549@outlook.com>
2024-02-27 00:44:20 +0900

    feat: create DIctionary get, set

---
+++ JsonParser/JsonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonParser {
+    public class JsonManager {
+        public static Dictionary<string, string>? Dics = new();
+        public static readonly string PATH = "./config.json";
+
+        public JsonManager() {
+                CreateConfigFile();
+                string jsonString = File.ReadAllText(PATH);
+                Dics = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+            }
+
+        ~JsonManager() {
+            string json = JsonSerializer.Serialize<Dictionary<string, string>>(Dics);
+            File.WriteAllText(PATH, json);
+        }
+        
+        public void CreateConfigFile() {
+            if(!File.Exists(PATH)) File.Create(PATH);
+        }
+        
+        public string GetValueByKey(string key) {
+            return Dics[key];
+        }
+        
+        public void SetValueByKey(string key, string value) {
+            Dics.Add(key, value);
+            // Set 하면서 동기화 하는건가?
+        }
+        
+        
+    }
+}
